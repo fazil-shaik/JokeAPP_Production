a1407566abfec6051d881215bd1dafe731f5c7d6
shaik fazil basha <93812181+fazil-shaik@users.noreply.github.com>
2023-12-30 09:32:30 +0530

    Update vite.config.js
    updated vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
       proxy: {
-      '/api':'http://localhost:3000',
+      '/api':'https://deployment-server-l3be3gvb0-shaik-fazil-bashas-projects.vercel.app/',
       },
       },
   plugins: [react()],
